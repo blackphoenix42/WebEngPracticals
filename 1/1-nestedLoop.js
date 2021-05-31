85991d6efdbad279298d76e656303ab825ae29ec
Ayush Yadav <aayush.sang@gmail.com>
2021-05-31 14:05:02 +0530

    Demonstrate Nested Loop

---
+++ 1/1-nestedLoop.js
@@ -0,0 +1,9 @@
+// Using javascript demonstrate Nested loop.
+
+arr = [['hello', 'world!'], ['How', 'are', 'you', 'doing?']]
+
+for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j])
+    }
+}
